de3b53ac3857fa29142493b35fc6afaacf42cdf2
Franco Madariaga <frankurox@gmail.com>
2020-09-09 18:16:16 -0300

    Agregar la base de la calculadora 2
    Agregar operaciones con decimales
    Agregar ingresar numero

--- Fuentes/CalculadoraApp/app/src/main/java/cl/inacap/calculadoraapp/MainActivity.java
+++ Fuentes/CalculadoraApp/app/src/main/java/cl/inacap/calculadoraapp/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
     private EditText n1Txt;
     private EditText n2Txt;
 
@@ -17,27 +18,54 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         this.n1Txt = findViewById(R.id.n1Txt);
         this.n2Txt = findViewById(R.id.n2Txt);
+
     }
 
-    public void sumar(View view){
-        int n1 = Integer.parseInt(this.n1Txt.getText().toString());
-        int n2 = Integer.parseInt(this.n2Txt.getText().toString());
-        Toast.makeText(this,"la suma es "+(n1+n2),Toast.LENGTH_SHORT).show();
+    public void sumar(View view) {
+        try {
+            double n1 = Double.parseDouble(this.n1Txt.getText().toString());
+            double n2 = Double.parseDouble(this.n2Txt.getText().toString());
+            Toast.makeText(this, "la suma es " + (n1 + n2), Toast.LENGTH_SHORT).show();
+        } catch (Exception ex) {
+            Toast.makeText(this, "Debe ingresar numeros", Toast.LENGTH_SHORT).show();
+        }
+
+
     }
-    public void restar(View view){
-        int n1 = Integer.parseInt(this.n1Txt.getText().toString());
-        int n2 = Integer.parseInt(this.n2Txt.getText().toString());
-        Toast.makeText(this,"la resta es "+(n1-n2),Toast.LENGTH_SHORT).show();
+
+    public void restar(View view) {
+        try {
+            double n1 = Double.parseDouble(this.n1Txt.getText().toString());
+            double n2 = Double.parseDouble(this.n2Txt.getText().toString());
+            Toast.makeText(this, "la resta es " + (n1 - n2), Toast.LENGTH_SHORT).show();
+        } catch (Exception ex) {
+            Toast.makeText(this, "Debe ingresar numeros", Toast.LENGTH_SHORT).show();
+        }
+
+
     }
-    public void multiplicar(View view){
-        int n1 = Integer.parseInt(this.n1Txt.getText().toString());
-        int n2 = Integer.parseInt(this.n2Txt.getText().toString());
-        Toast.makeText(this,"la multiplicacion es "+(n1*n2),Toast.LENGTH_SHORT).show();
+
+    public void multiplicar(View view) {
+        try {
+            double n1 = Double.parseDouble(this.n1Txt.getText().toString());
+            double n2 = Double.parseDouble(this.n2Txt.getText().toString());
+            Toast.makeText(this, "la multiplicacion es " + (n1 * n2), Toast.LENGTH_SHORT).show();
+
+
+        } catch (Exception ex) {
+            Toast.makeText(this, "Debe ingresar numeros", Toast.LENGTH_SHORT).show();
+        }
+
     }
-    public void dividir(View view){
-        int n1 = Integer.parseInt(this.n1Txt.getText().toString());
-        int n2 = Integer.parseInt(this.n2Txt.getText().toString());
-        double division = n1/n2;
-        Toast.makeText(this,"la division es "+division,Toast.LENGTH_SHORT).show();
+
+    public void dividir(View view) {
+        try {
+            double n1 = Double.parseDouble(this.n1Txt.getText().toString());
+            double n2 = Double.parseDouble(this.n2Txt.getText().toString());
+            Toast.makeText(this, "la division es " + n1 / n2, Toast.LENGTH_SHORT).show();
+        } catch (Exception ex) {
+            Toast.makeText(this, "Debe ingresar numeros", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
